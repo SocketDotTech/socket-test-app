89c07461395b464e3dbf95606c93880264aa31b6
Rookmate <14072042+rookmate@users.noreply.github.com>
2025-03-17 13:33:01 +0400

    Rename variable to counter

--- src/forwarder-on-evmx/Counter.sol
+++ src/forwarder-on-evmx/Counter.sol
@@ -1,10 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+/**
+ * @title Counter
+ * @dev A simple contract that maintains a counter value which can be incremented
+ * Used for multi-chain communication testing via SOCKET Protocol
+ */
 contract Counter {
-    uint256 public number;
+    /**
+     * @notice The current counter value
+     * @dev Public variable that can be read directly or through the getter function
+     */
+    uint256 public counter;
 
+    /**
+     * @notice Increases the counter value by 1
+     * @dev This function modifies the state by incrementing the counter variable
+     */
     function increment() public {
-        number++;
+        counter++;
     }
 }
