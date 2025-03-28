565e1ed17111973f18576f37a93f4300ee0abecf
Rookmate <14072042+rookmate@users.noreply.github.com>
2025-03-28 14:17:46 +0000

    Temporary - pointing to lib/socket-protocol dev branch

--- script/update-socket-protocol-commit.sh
+++ script/update-socket-protocol-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check for modified or staged files, but ignore untracked files
-if [[ -n $(git status --porcelain | grep '^[ MRAUCD]') ]]; then
+if git status --porcelain | grep -q '^[ MRAUCD]'; then
   echo "Error: You have modified or staged files. Commit or stash them before running this script."
   exit 1
 fi
@@ -12,10 +12,10 @@ rm -rf .git/modules/lib/socket-protocol
 git rm -rf lib/socket-protocol
 
 # Re-add the submodule
-git submodule add -b staging https://github.com/SocketDotTech/socket-protocol.git lib/socket-protocol
+git submodule add -b dev https://github.com/SocketDotTech/socket-protocol.git lib/socket-protocol
 git submodule update --init --recursive
 
 # Commit the update
-git commit -m "Update socket-protocol submodule"
+#git commit -m "Update socket-protocol submodule"
 
 echo "SOCKET Protocol submodule updated successfully!"
