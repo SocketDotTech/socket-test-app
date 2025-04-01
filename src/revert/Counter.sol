c5a45a2f4607983c7cb0707884a3c4cb5d74a23c
Rookmate <14072042+rookmate@users.noreply.github.com>
2025-04-01 13:35:26 +0100

    Fix onchain contract

--- src/revert/Counter.sol
+++ src/revert/Counter.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "socket-protocol/contracts/base/PlugBase.sol";
+
 /**
  * @title Counter
  * @dev A simple contract that maintains a counter value which can be incremented
  * Used for multi-chain communication testing via SOCKET Protocol
  */
-contract Counter {
+contract Counter is PlugBase {
     /**
      * @notice The current counter value
      * @dev Public variable that can be read directly or through the getter function
@@ -19,5 +21,16 @@ contract Counter {
      */
     function increment() public {
         counter++;
+    }
+
+    /**
+     * @notice Connects the contract to the SOCKET Protocol
+     * @dev Sets up the contract for EVMx communication by calling the parent PlugBase method
+     * @param appGateway_ Address of the application gateway contract
+     * @param socket_ Address of the SOCKET Protocol contract
+     * @param switchboard_ Address of the switchboard contract
+     */
+    function connectSocket(address appGateway_, address socket_, address switchboard_) external onlySocket {
+        _connectSocket(appGateway_, socket_, switchboard_);
     }
 }
