c5dda8ab3da3e6b9fcfb38a0eda1de53016da2d4
Rookmate <14072042+rookmate@users.noreply.github.com>
2025-06-02 10:59:10 +0100

    chore: change DeliveryHelper.t.sol to SetupTest.t.sol

--- test/Dummy.t.sol
+++ test/Dummy.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.21;
 
-import "socket-protocol/test/DeliveryHelper.t.sol";
+import "socket-protocol/test/SetupTest.t.sol";
 
-contract DummyTest is DeliveryHelperTest {
+contract DummyTest is AppGatewayBaseSetup {
     function testDummy() external {}
 }
